f227f5fa3ad528f6c232e9f32b1b5e499af8de87
Kristina Demidova <kristi200483@gmail.com>
2022-11-07 19:18:17 +0300

    Применила декоратор к функции

--- main.py
+++ main.py
@@ -1,21 +1,32 @@
 from flask import Flask, render_template, request, make_response
 import datetime as dt
+def toweb(f):
+    app = Flask(__name__)
 
-app = Flask(__name__)
+    @app.route("/", methods=['GET', 'POST'])
+    def myfunc():
+        about = f.__doc__
+        a = f.__annotations__
+        if 'return' in a:
+            a.pop('return')
 
+        if request.form:
+            res = f(**request.form)
+        else:
+            res = None
 
-@app.route('/')
+        return render_template('index.html', about=about, a=a, res=res)
 
-@app.route('/index', methods=['POST', 'GET'])
+    return app
 
-def index():
-    answer = ""
-    resp = make_response(render_template('index.html'))
-    if (request.method == 'POST'):
-        a = float(request.form['select'])
-        delta_time = dt.timedelta(hours=a)
-        answer = str((dt.datetime.now() + delta_time).strftime("%d-%m-%Y %H:%M:%S"))
-    return render_template('index.html', answer=answer)
+@toweb
+def index(a: int) -> str:
+    """
+    Эта функция считает, сколько сейчас времени в каком-то часовом поясе России. Пожалуйста, введите разницу часового пояса с Москвой.
+    """
+    a = float(a)
+    delta_time = dt.timedelta(hours=a)
+    answer = str((dt.datetime.now() + delta_time).strftime("%d-%m-%Y %H:%M:%S"))
+    return answer
 
-if __name__ == "__main__":
-    app.run()
+index.run(host='0.0.0.0', port="5001")
